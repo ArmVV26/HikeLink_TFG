62efbd5c1855089e9b9395fd29befdadc23d7953
Armando Vaquero Vargas <ditovaquero@gmail.com>
2025-06-11 09:14:45 +0200

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -46,3 +46,4 @@ urlpatterns = [
 # Funciona SOLO en desarrollo (no en producción)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
